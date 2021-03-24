ce5e3f7c263b9b20823328dc854a31ba24667e7c
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-03-24 04:48:28 -0300

    .env fix and adds helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import helmet from "helmet"
 import { config as dotenv } from "dotenv"
 import { router } from "./routes"
 
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
 app.use(router)
 
 dotenv({ path: `${__dirname}/config/.env` })
