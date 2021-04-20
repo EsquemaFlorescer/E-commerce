2a914fd01ad27c3f9035fda6d45ecaec032f02d2
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-20 13:38:34 -0300

    Adds UTC Timestamp + adds 4 digits in UUID

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -9,15 +9,15 @@ interface CreateUserDTO {
 
 class User {
   public readonly uuid: string
-  public readonly created_at: string
+  public readonly created_at: Date
 
   public name: string
   public email: string
   public password: string
 
   constructor(props: CreateUserDTO) {
-    this.uuid = randomBytes(20).toString("hex")
-    this.created_at = new Date(Date.now()).toLocaleDateString()
+    this.uuid = randomBytes(24).toString("hex")
+    this.created_at = new Date()
 
     Object.assign(this, props)
 
