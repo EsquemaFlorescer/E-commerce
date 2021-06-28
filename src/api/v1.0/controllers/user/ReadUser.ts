4b5b35f0cd6b216801f05137a98f311b172f2934
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-06-28 07:34:09 -0300

    changes read user function to class

--- src/api/v1.0/controllers/user/ReadUser.ts
+++ src/api/v1.0/controllers/user/ReadUser.ts
@@ -1,115 +1,126 @@
 import { Request, Response } from "express"
 import { ParsedQs } from "qs"
 
-import { User } from "@prisma/client"
-import { prisma } from "@src/prisma"
+import { IUsersRepository } from "@api/v1.0/repositories/IUsersRepository"
+import { SqliteUsersRepository } from "@api/v1.0/repositories/implementations/SqliteUsersRepository"
+
+import { User } from "@api/v1.0/entities/User"
 
 type readUserResponse = {
   users: User[]
 }
 
-const read = async (id: string, { query }: Request<ParsedQs>) => {
-  try {
-    // listing options
-    let page: number = Number(query.page)
-    let quantity: number = Number(query.quantity)
+export class ReadUserService {
+  constructor(
+    private usersRepository: IUsersRepository
+  ) {}
 
-    let name: string = String(query.name)
-    let sort: any = String(query.sort)
-    let created_at: string = String(query.createdAt)
-    
-    // if id is supplied search user with that id
-    if(id != undefined) {
-      const users = await prisma.user.findUnique({
-        where: {
-          id
+  async read(id: string, { query }: Request<ParsedQs>) {
+    try {
+      // listing options
+      let page: number = Number(query.page)
+      let quantity: number = Number(query.quantity)
+  
+      let name: string = String(query.name)
+      let sort: any = String(query.sort)
+      let created_at: string = String(query.createdAt)
+      
+      // if id is supplied search user with that id
+      if(id != undefined) {
+        const users = await this.usersRepository.findById(id)
+  
+        return {
+          users
         }
-      })
-
-      return {
-        users
       }
-    }
-
-    // if no page and no quantity is supplied, list all users
-    if(!page && !quantity) {
-      const users = await prisma.user.findMany()
-
-      return {
-        users
+  
+      // if no page and no quantity is supplied, list all users
+      if(!page && !quantity) {
+        const users = await this.usersRepository.findAllUsers()
+  
+        return {
+          users
+        }
       }
-    }
-
-    //
-    if(name != "undefined") {
-      // search for users with "x" name and sort it
-      if(sort != "undefined") {
-        const users = await prisma.user.findMany({
-          orderBy: [{
-            name: sort
-          }],
-          
+  
+      //
+      if(name != "undefined") {
+        // search for users with "x" name and sort it
+        if(sort != "undefined") {
+          const users = await this.usersRepository.findAllUsers({
+            orderBy: [{
+              name: sort
+            }],
+            
+            // pagination
+            take: quantity,
+            skip: page * quantity
+          })
+  
+          return { 
+            users
+          }
+        }
+  
+        // search for users with "x" name
+        const users = await this.usersRepository.findAllUsers({
+          where: {
+            name: {
+              contains: name
+            }
+          },
+  
           // pagination
           take: quantity,
           skip: page * quantity
         })
-
-        return { 
+  
+        return {
           users
         }
       }
-
-      // search for users with "x" name
-      const users = await prisma.user.findMany({
-        where: {
-          name: {
-            contains: name
-          }
-        },
-
-        // pagination
+  
+      // if no name is supplied and created_at is supplied, sort by created_at
+      if(name == "undefined" && created_at == "true" && sort != "undefined") {
+        const users = await this.usersRepository.findAllUsers({
+          orderBy: [{
+            created_at: sort
+          }],
+  
+          take: quantity,
+          skip: page * quantity
+        })
+  
+        return {
+          users
+        }
+      }
+  
+      // if only page and quantity are supplied, sort users with pagination
+      const users = await this.usersRepository.findAllUsers({
         take: quantity,
         skip: page * quantity
       })
-
+  
       return {
         users
       }
+    } catch (error) {
+      return error
     }
-
-    // if no name is supplied and created_at is supplied, sort by created_at
-    if(name == "undefined" && created_at == "true" && sort != "undefined") {
-      const users = await prisma.user.findMany({
-        orderBy: [{
-          created_at: sort
-        }],
-
-        take: quantity,
-        skip: page * quantity
-      })
-
-      return {
-        users
-      }
-    }
-
-    // if only page and quantity are supplied, sort users with pagination
-    const users = await prisma.user.findMany({
-      take: quantity,
-      skip: page * quantity
-    })
-
-    return {
-      users
-    }
-  } catch (error) {
-    return error
   }
 }
 
 export default async (request: Request, response: Response) => {
   try {
-    const { users }: readUserResponse = await read(request.params.id, request)
+    // create sqlite repository
+    const sqliteUsersRepository = new SqliteUsersRepository()
+
+    // create read user service
+    const readUser = new ReadUserService(sqliteUsersRepository)
+
+    // execute user service
+    const { users }: readUserResponse = await readUser.read(request.params.id, request)
 
     // respond with user information
     return ({
