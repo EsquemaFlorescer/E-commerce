98b0d4e7b7de7377a2b27e280dae42a028a0293f
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-03 01:18:41 -0300

    add: new middleware that checks for dashboard authorization header

--- src/api/v1.0/routes/middlewares/dashAuth.ts
+++ src/api/v1.0/routes/middlewares/dashAuth.ts
@@ -4,23 +4,18 @@ import { Request, Response, NextFunction } from "express"
 
 export default (request: Request, response: Response, next: NextFunction) => {
   try {
-    // get JWT refresh token secret
-    const JWT_REFRESH_TOKEN = String(process.env.JWT_REFRESH_TOKEN)
+    const DASH_REFRESH_TOKEN = String(process.env.DASH_REFRESH_TOKEN)
 
-    // get JWT refresh token from headers
     const authHeader = request.headers.authorization
-    // remove Bearer prefix from token
     const token = String(authHeader && authHeader.split(" ")[1])
 
-    verify(token, JWT_REFRESH_TOKEN)
+    verify(token, DASH_REFRESH_TOKEN)
 
     !authHeader && (
       response.status(400).json({
-        message: "No JWT refresh token was found! Redirect to login."
+        message: "No Dash JWT refresh token was found! Redirect to login."
       })
     )
-
-    next()
   } catch (error) {
     return response.json("Failed to verify Admin user JWT token.")
   }
