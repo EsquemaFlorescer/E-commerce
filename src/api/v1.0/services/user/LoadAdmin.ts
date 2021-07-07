3d8d03e8fe33a4b6e2dd2e5e1a6b500207e2a18e
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-07 00:23:02 -0300

    fix: major refactor of load admin users service

--- src/api/v1.0/services/user/LoadAdmin.ts
+++ src/api/v1.0/services/user/LoadAdmin.ts
@@ -1,86 +1,70 @@
-import { Request } from "express"
+import { Request } from 'express';
 
-import { IUsersRepository } from "@v1/repositories"
-import { SqliteUsersRepository } from "@v1/repositories/implementations"
+import { IUsersRepository } from '@v1/repositories';
+import { SqliteUsersRepository } from '@v1/repositories/implementations';
 
-import { User } from "@v1/entities"
-import auth from "@v1/auth"
+import { User } from '@v1/entities';
+import auth from '@v1/auth';
 
 class LoadAdminService {
-  constructor(
-    private usersRepository: IUsersRepository
-  ) {}
+	constructor(private usersRepository: IUsersRepository) {}
 
-  async load(users: User[] | User) {
-    try {
-      if(Array.isArray(users)) {
-        const user = users.forEach(async (user: User) => {
-          const newUser = new User(user, {
-            admin: {
-              username: user.username,
-              userhash: user.userhash
-            }
-          })
+	async load(users: User[]) {
+		try {
+			for (let user of users) {
+				const userAlreadyExists = await this.usersRepository.findByEmail(
+					user.email
+				);
 
-          const access_token = auth.create(newUser, "24h")
-  
-          await this.usersRepository.save(newUser)
+				if (userAlreadyExists.length) {
+					throw new Error(
+						'This user already exists or have already been loaded.'
+					);
+				}
 
-          return {
-            id: newUser.id,
-            email: newUser.email,
-            access_token
-          }
-        })
+				const newUser = new User(user, {
+					admin: {
+						username: user.username,
+						userhash: user.userhash,
+					},
+				});
 
-        return ({
-          user
-        })
-      }
+				const access_token = auth.create(newUser, '24h');
 
-      const newUser = new User(users, {
-        admin: {
-          username: users.username,
-          userhash: users.userhash
-        }
-      })
+				await this.usersRepository.save(newUser);
 
-      await this.usersRepository.save(newUser)
-      
-      const access_token = auth.create(newUser, "24h")
-
-      const user = {
-        id: newUser.id,
-        email: newUser.email,
-        access_token
-      }
-
-      return ({
-        user
-      })
-    } catch (error) {
-      throw new Error(error.message)
-    }
-  }
+				return {
+					user: {
+						id: newUser.id,
+						email: newUser.email,
+						access_token,
+					},
+				};
+			}
+		} catch (error) {
+			throw new Error(error.message);
+			return error;
+		}
+	}
 }
 
 export default async (request: Request) => {
-  try {
-    const usersRepository = new SqliteUsersRepository()
-    const LoadAdmin = new LoadAdminService(usersRepository)
+	try {
+		const usersRepository = new SqliteUsersRepository();
+		const LoadAdmin = new LoadAdminService(usersRepository);
 
-    const { user } = await LoadAdmin.load(request.body)
+		const { user } = await LoadAdmin.load(request.body);
 
-    return ({
-      status: 200,
-      user,
-      message: "Loaded admin users from file."
-    })
-  } catch (error) {
-    return ({
-      error: true,
-      status: 400,
-      message: "Failed to load admin users."
-    })
-  }
-}
+		return {
+			status: 200,
+			user,
+			message: 'Loaded admin users from file.',
+		};
+	} catch (error) {
+		return {
+			error: true,
+			status: 400,
+			message: error.message,
+		};
+	}
+};
