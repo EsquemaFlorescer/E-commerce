d6d0064c0701bdcca9f29a02d0cc0d6b8f2d6c60
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-07 00:24:34 -0300

    fix: dash authentication not working

--- src/api/v1.0/routes/middlewares/dashAuth.ts
+++ src/api/v1.0/routes/middlewares/dashAuth.ts
@@ -1,22 +1,22 @@
-import { verify } from "jsonwebtoken"
+import { verify } from 'jsonwebtoken';
 
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from 'express';
 
 export default (request: Request, response: Response, next: NextFunction) => {
-  try {
-    const DASH_REFRESH_TOKEN = String(process.env.DASH_REFRESH_TOKEN)
+	try {
+		const DASH_REFRESH_TOKEN = String(process.env.DASH_REFRESH_TOKEN);
+		const authHeader = request.headers.authorization;
+		const token = String(authHeader && authHeader.split(' ')[1]);
 
-    const authHeader = request.headers.authorization
-    const token = String(authHeader && authHeader.split(" ")[1])
+		verify(token, DASH_REFRESH_TOKEN);
 
-    verify(token, DASH_REFRESH_TOKEN)
+		!authHeader &&
+			response.status(400).json({
+				message: 'No Dash JWT refresh token was found! Redirect to login.',
+			});
 
-    !authHeader && (
-      response.status(400).json({
-        message: "No Dash JWT refresh token was found! Redirect to login."
-      })
-    )
-  } catch (error) {
-    return response.status(401).json("Failed to verify Admin user JWT token.")
-  }
-}
+		next();
+	} catch (error) {
+		return response.status(401).json('Failed to verify Admin user JWT token.');
+	}
+};
