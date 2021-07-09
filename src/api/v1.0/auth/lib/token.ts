25eaf385569f4d2c281b443b1ee15cb0dcff0e59
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-09 12:50:09 -0300

    :package: add: new JWT token methods to auth lib

--- src/api/v1.0/auth/lib/token.ts
+++ src/api/v1.0/auth/lib/token.ts
@@ -1,19 +1,35 @@
-import { sign } from 'jsonwebtoken';
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
 
 type userType = {
 	id: string;
 	token_version: number;
 };
 
-export const createToken = (
-	{ id, token_version }: userType,
-	expiresIn: string
-) => {
-	const JWT_ACCESS_TOKEN: string = String(process.env.JWT_ACCESS_TOKEN);
+export const createAccessToken = (info: userType, expiresIn: string) => {
+	const access_token_secret: string = String(process.env.JWT_ACCESS_TOKEN);
 
-	const access_token = sign({ id, token_version }, JWT_ACCESS_TOKEN, {
-		expiresIn,
-	});
-
+	const access_token = sign(info, access_token_secret, { expiresIn });
 	return access_token;
+};
+
+export const createRefreshToken = (info: userType, expiresIn: string) => {
+	const refresh_token_secret: string = String(process.env.JWT_REFRESH_TOKEN);
+
+	const refresh_token = sign(info, refresh_token_secret, { expiresIn });
+	return refresh_token;
+};
+
+export const verifyToken = (token: string, type: string): string | JwtPayload => {
+	const access_token_secret: string = String(process.env.JWT_ACCESS_TOKEN);
+	const refresh_token_secret: string = String(process.env.JWT_REFRESH_TOKEN);
+
+	if (type == 'access') {
+		const payload = verify(token, access_token_secret);
+
+		return payload;
+	}
+
+	const payload = verify(token, refresh_token_secret);
+
+	return payload;
 };
