0c0595499dd24abe7d86f7745e688397ec95781b
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-11 19:02:52 -0300

    :package: add: body parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { urlencoded, json } from 'body-parser';
 import helmet from 'helmet';
 import { config as dotenv } from 'dotenv';
 import { router as v1 } from '@v1/routes';
@@ -12,7 +13,8 @@ router.use('/v1', v1);
 
 dotenv({ path: '.env' });
 
-app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 app.use(cors({ origin: process.env.APP_URL }));
 app.use(helmet());
 app.use(router);
