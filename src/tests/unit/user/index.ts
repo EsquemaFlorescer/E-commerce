7fa701db6ee1cf4a0dde1a41e42d1208f68ac774
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-11 19:14:17 -0300

    add: forgot password test

--- src/tests/unit/user/index.ts
+++ src/tests/unit/user/index.ts
@@ -2,7 +2,17 @@ import { CreateUserTest } from './CreateUser';
 import { ReadUserTest } from './ReadUser';
 import { UpdateUserTest } from './UpdateUser';
 import { DeleteUserTest } from './DeleteUser';
+import { forgetPasswordTest } from './ForgetPassword';
+
 import { AddressTest } from './Address';
 import { CartTest } from './Cart';
 
-export { CreateUserTest, ReadUserTest, UpdateUserTest, DeleteUserTest, AddressTest, CartTest };
+export {
+	CreateUserTest,
+	ReadUserTest,
+	UpdateUserTest,
+	DeleteUserTest,
+	AddressTest,
+	CartTest,
+	forgetPasswordTest,
+};
