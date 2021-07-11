7e26c2a2cc825b5beb354be3141c6fa2072ac2ba
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-10 22:42:41 -0300

    add: Cart tests

--- src/tests/all.test.ts
+++ src/tests/all.test.ts
@@ -1,13 +1,19 @@
-import { CreateUserTest, ReadUserTest, UpdateUserTest, DeleteUserTest } from './unit/user';
+import {
+	CreateUserTest,
+	ReadUserTest,
+	UpdateUserTest,
+	DeleteUserTest,
+	AddressTest,
+	CartTest,
+} from './unit/user';
 import { emailTest, usernameTest, tokenTest } from './unit/auth';
-import { CreateAddressTest } from './unit/address';
+import { IPTest } from './unit/IpBan';
+
 import { prisma } from '@src/prisma';
-
-beforeAll(async () => {
-	await prisma.user.deleteMany();
+afterEach(async () => {
+	await prisma.address.deleteMany();
+	await prisma.cart.deleteMany();
 });
-
-// describe('Create Addres', CreateAddressTest);
 describe('token creation', tokenTest);
 describe('username auth', usernameTest);
 describe('email auth', emailTest);
@@ -16,3 +22,7 @@ describe('Create user', CreateUserTest);
 describe('Read user', ReadUserTest);
 describe('Update user', UpdateUserTest);
 describe('Delete user', DeleteUserTest);
+
+describe('Create Address', AddressTest);
+describe('Create Address', CartTest);
+describe('IP ban', IPTest);
--- src/tests/unit/user/index.ts
+++ src/tests/unit/user/index.ts
@@ -2,5 +2,7 @@ import { CreateUserTest } from './CreateUser';
 import { ReadUserTest } from './ReadUser';
 import { UpdateUserTest } from './UpdateUser';
 import { DeleteUserTest } from './DeleteUser';
+import { AddressTest } from './Address';
+import { CartTest } from './Cart';
 
-export { CreateUserTest, ReadUserTest, UpdateUserTest, DeleteUserTest };
+export { CreateUserTest, ReadUserTest, UpdateUserTest, DeleteUserTest, AddressTest, CartTest };
