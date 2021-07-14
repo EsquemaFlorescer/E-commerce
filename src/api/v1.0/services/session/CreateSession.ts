3dd5acb90d1ca76681bf5e687ed71629d38db9af
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-14 19:09:24 -0300

    fix: add nullable type to create session login request type

--- src/api/v1.0/services/session/CreateSession.ts
+++ src/api/v1.0/services/session/CreateSession.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express';
 import { compare } from 'bcrypt';
-import { verify, sign } from 'jsonwebtoken';
 
 import { SqliteUsersRepository } from '@v1/repositories/implementations';
 import { IUsersRepository } from '@v1/repositories';
@@ -12,9 +11,9 @@ import { isBanned } from '@v1/utils/IsBanned';
 import auth from '@v1/auth';
 
 type loginRequestType = {
-	email?: string;
-	username?: string;
-	userhash?: string;
+	email: string;
+	username: string;
+	userhash: string;
 	password: string;
 };
 
@@ -65,7 +64,7 @@ class CreateSessionService {
 				refresh_token: token,
 			};
 		} catch (error) {
-			if (error.message == 'Your banned. If you think this is a mistake contact our team.') {
+			if (error.message == 'You are banned. If you think this is a mistake contact our team.') {
 				throw new Error(error.message);
 			}
 			if (error.message == "Sorry, we couldn't complete your request, please try again later.") {
@@ -75,16 +74,19 @@ class CreateSessionService {
 				throw new Error(error.message);
 			}
 
+			if (loginRequest.email == undefined) throw new Error('please insert e-mail');
+			if (loginRequest.username == undefined && loginRequest.userhash == undefined)
+				throw new Error('please insert username and userhash');
+
 			if (loginRequest.email == undefined) {
 				// find user
 				const { user, failed_too_many, matchPassword } = await auth.loginUsername(loginRequest);
-
 				if (failed_too_many) {
 					const { name, email, failed_attemps } = user;
 
 					await this.usersRepository.update(user);
 
-					await this.mailProvider.sendMail({
+					this.mailProvider.sendMail({
 						to: {
 							name,
 							email,
@@ -130,7 +132,7 @@ class CreateSessionService {
 
 				await this.usersRepository.update(user);
 
-				await this.mailProvider.sendMail({
+				this.mailProvider.sendMail({
 					to: {
 						name,
 						email,
