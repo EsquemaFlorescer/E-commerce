25b13c32fd6fee7b3867b3bf559adefc868ce2fd
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-14 19:08:46 -0300

    chore: updates typo in isBanned function

--- src/api/v1.0/utils/IsBanned.ts
+++ src/api/v1.0/utils/IsBanned.ts
@@ -3,14 +3,10 @@ export const isBanned = (
 	shadow_ban: boolean | null | undefined
 ) => {
 	if (shadow_ban === true) {
-		throw new Error(
-			"Sorry, we couldn't complete your request, please try again later."
-		);
+		throw new Error("Sorry, we couldn't complete your request, please try again later.");
 	}
 
 	if (ban === true) {
-		throw new Error(
-			`Your banned. If you think this is a mistake contact our team.`
-		);
+		throw new Error(`You are banned. If you think this is a mistake contact our team.`);
 	}
 };
