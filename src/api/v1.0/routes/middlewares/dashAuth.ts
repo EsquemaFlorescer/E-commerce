0c3c385e48c4c65d394b985b8a78d84672859448
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-16 12:11:17 -0300

    :bug: fix: error where undefined token was not returning error message

--- src/api/v1.0/routes/middlewares/dashAuth.ts
+++ src/api/v1.0/routes/middlewares/dashAuth.ts
@@ -6,17 +6,16 @@ export default (request: Request, response: Response, next: NextFunction) => {
 	try {
 		const DASH_REFRESH_TOKEN = String(process.env.DASH_REFRESH_TOKEN);
 		const authHeader = request.headers.authorization;
-		const token = String(authHeader && authHeader.split(' ')[1]);
-
-		verify(token, DASH_REFRESH_TOKEN);
-
 		!authHeader &&
 			response.status(400).json({
 				message: 'No Dash JWT refresh token was found! Redirect to login.',
 			});
+		const token = String(authHeader && authHeader.split(' ')[1]);
+
+		verify(token, DASH_REFRESH_TOKEN);
 
 		next();
 	} catch (error) {
-		return response.status(401).json('Failed to verify Admin user JWT token.');
+		return response.status(401).json(error.message);
 	}
 };
