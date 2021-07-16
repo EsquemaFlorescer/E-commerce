b261f522a6e2a846fec813695320513f649a71cf
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-16 12:31:13 -0300

    chore: lints session tests

--- src/tests/v1/controllers/Session.test.ts
+++ src/tests/v1/controllers/Session.test.ts
@@ -13,13 +13,13 @@ describe('Session controller', () => {
 		id: '',
 		name: 'test',
 		email: 'test@test.com',
-		username: "",
-		userhash: "",
+		username: '',
+		userhash: '',
 		password: '123',
 		access: '',
 	};
 
-	it("should activate a user", async () => {
+	it('should activate a user', async () => {
 		const token = jwt.sign(userInfo, jwt_access_token_secret);
 		const activate: ApiResponse<User> = await request(app)
 			.post('/v1/user/activate')
@@ -28,9 +28,9 @@ describe('Session controller', () => {
 		userInfo.id = activate.body.user.id;
 		userInfo.access = activate.body.access_token;
 
-		userInfo.username = activate.body.user.username
-		userInfo.userhash = activate.body.user.userhash
-	})
+		userInfo.username = activate.body.user.username;
+		userInfo.userhash = activate.body.user.userhash;
+	});
 
 	it('should login user with JWT', async () => {
 		const login: ApiResponse<User> = await request(app)
@@ -43,54 +43,74 @@ describe('Session controller', () => {
 	});
 
 	it('should login user with login user with e-mail', async () => {
-		const { email, password } = userInfo
-		const login: ApiResponse<User> = await request(app).post("/v1/user/login").send({ email, password })
+		const { email, password } = userInfo;
+		const login: ApiResponse<User> = await request(app)
+			.post('/v1/user/login')
+			.send({ email, password });
 
-		expect(login.status).toBe(200)
+		expect(login.status).toBe(200);
 
-		expect(login.body.social_login).toBe(true)
-		expect(login.body.refresh_token.length).toBeGreaterThan(10)
+		expect(login.body.social_login).toBe(true);
+		expect(login.body.refresh_token.length).toBeGreaterThan(10);
 	});
 
 	it('should not be able to login user with wrong e-mail', async () => {
-		const { password } = userInfo
-		const login: ApiResponse<void> = await request(app).post("/v1/user/login").send({ email: "test@jest.com", password })
+		const { password } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ email: 'test@jest.com', password });
 
-		expect(login.status).toBe(400)
-		expect(login.body).toBe("Wrong e-mail!")
+		expect(login.status).toBe(400);
+		expect(login.body).toBe('Wrong e-mail!');
 	});
 
 	it('should not be able to e-mail login user with wrong password', async () => {
-		const { email } = userInfo
-		const login: ApiResponse<void> = await request(app).post("/v1/user/login").send({ email, password: "test" })
+		const { email } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ email, password: 'test' });
 
-		expect(login.status).toBe(400)
-		expect(login.body).toBe("Wrong password!")
+		expect(login.status).toBe(400);
+		expect(login.body).toBe('Wrong password!');
 	});
 
 	it('should login user with login user with username', async () => {
-		const { username, userhash, password } = userInfo
-		const login: ApiResponse<void> = await request(app).post('/v1/user/login').send({ username, userhash, password })
+		const { username, userhash, password } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ username, userhash, password });
 
-		expect(login.status).toBe(200)
-		expect(login.body.social_login).toBe(true)
-		expect(login.body.refresh_token.length).toBeGreaterThan(10)
+		expect(login.status).toBe(200);
+		expect(login.body.social_login).toBe(true);
+		expect(login.body.refresh_token.length).toBeGreaterThan(10);
 	});
 
 	it('should not be able to login user with wrong username', async () => {
-		const { userhash, password } = userInfo
-		const login: ApiResponse<void> = await request(app).post("/v1/user/login").send({ username: "jest", userhash, password })
+		const { userhash, password } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ username: 'jest', userhash, password });
 
-		expect(login.status).toBe(400)
-		expect(login.body).toBe("Wrong username!")
+		expect(login.status).toBe(400);
+		expect(login.body).toBe('Wrong username!');
 	});
 
 	it('should not be able to username login user with wrong password', async () => {
-		const { username, userhash } = userInfo
-		const login: ApiResponse<void> = await request(app).post('/v1/user/login').send({ username, userhash, password: "jest" })
+		const { username, userhash } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ username, userhash, password: 'jest' });
 
-		expect(login.status).toBe(400)
-		expect(login.body).toBe("Wrong password!")
+		expect(login.status).toBe(400);
+		expect(login.body).toBe('Wrong password!');
+	});
+
+	it('should not be able to login with invalid email', async () => {
+		const { password } = userInfo;
+		const login: ApiResponse<void> = await request(app)
+			.post('/v1/user/login')
+			.send({ email: 'email', password });
+		expect(login.body).toBe('Invalid email.');
 	});
 
 	beforeAll(async () => await clear());
