5aae0f2c57bbccdf0f60968c64564d91d379e3b7
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-15 23:34:32 -0300

    fix: typo in update item service

--- src/api/v1.0/services/item/UpdateItem.ts
+++ src/api/v1.0/services/item/UpdateItem.ts
@@ -1,47 +1,44 @@
-import { Request } from "express"
+import { Request } from 'express';
 
-import { IItemsRepository } from "@v1/repositories"
-import { SqliteItemsRepository } from "@v1/repositories/implementations"
+import { IItemsRepository } from '@v1/repositories';
+import { SqliteItemsRepository } from '@v1/repositories/implementations';
 
-import { Item } from "@v1/entities"
+import { Item } from '@v1/entities';
 
 class UpdateItemService {
-  constructor(
-    private itemsRepository: IItemsRepository
-  ) {}
+	constructor(private itemsRepository: IItemsRepository) {}
 
-  async read(id: number, updateItemRequest: Item) {
-    try {
-      const item = await this.itemsRepository.update(id, updateItemRequest)
+	async update(id: number, updateItemRequest: Item) {
+		try {
+			const item = await this.itemsRepository.update(id, updateItemRequest);
 
-      return {
-        item
-      }
-    } catch (error) {
-      throw new Error(error.message)
-    }
-  }
+			return {
+				item,
+			};
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
 }
 
 export default async (request: Request) => {
-  try {
-    const ItemsRepository = new SqliteItemsRepository()
-    const UpdateItem = new UpdateItemService(ItemsRepository)
+	try {
+		const ItemsRepository = new SqliteItemsRepository();
+		const UpdateItem = new UpdateItemService(ItemsRepository);
 
-    const { item } = await UpdateItem.read(Number(request.params.id), request.body)
-    
-    return ({
-      status: 202,
-      message: "Updated item with success!",
-      item
-    })
+		const { item } = await UpdateItem.update(Number(request.params.id), request.body);
 
-  } catch (error) {
-    // in case of error
-    return ({
-      error: true,
-      status: 400,
-      message: "Failed to update item."
-    })
-  }
-}
+		return {
+			status: 202,
+			message: 'Updated item with success!',
+			item,
+		};
+	} catch (error) {
+		// in case of error
+		return {
+			error: true,
+			status: 400,
+			message: 'Failed to update item.',
+		};
+	}
+};
