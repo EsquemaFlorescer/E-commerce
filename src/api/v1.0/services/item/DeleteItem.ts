efb69268c51c41fd528b923169cc1545311172c4
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-15 23:34:58 -0300

    :bug: fix: typo in delete item service

--- src/api/v1.0/services/item/DeleteItem.ts
+++ src/api/v1.0/services/item/DeleteItem.ts
@@ -1,38 +1,36 @@
-import { Request } from "express"
+import { Request } from 'express';
 
-import { IItemsRepository } from "@v1/repositories"
-import { SqliteItemsRepository } from "@v1/repositories/implementations"
+import { IItemsRepository } from '@v1/repositories';
+import { SqliteItemsRepository } from '@v1/repositories/implementations';
 
 class DeleteItemService {
-  constructor(
-    private itemsRepository: IItemsRepository
-  ) {}
+	constructor(private itemsRepository: IItemsRepository) {}
 
-  async delete(id: number) {
-    try {
-      await this.itemsRepository.delete(id)
-    } catch (error) {
-      throw new Error(error.message)
-    }
-  }
+	async delete(id: number) {
+		try {
+			await this.itemsRepository.delete(id);
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	}
 }
 
 export default async (request: Request) => {
-  try {
-    const ItemsRepository = new SqliteItemsRepository()
-    const DeleteItem = new DeleteItemService(ItemsRepository)
+	try {
+		const ItemsRepository = new SqliteItemsRepository();
+		const DeleteItem = new DeleteItemService(ItemsRepository);
 
-    await DeleteItem.delete(Number(request.params.id))
+		await DeleteItem.delete(Number(request.params.id));
 
-    return ({
-      status: 200,
-      message: "Item delete with success!"
-    })
-  } catch (error) {
-    return ({
-      error: true,
-      status: 400,
-      message: "Failed to delete item."
-    })
-  }
-}
+		return {
+			status: 200,
+			message: 'Item deleted with success!',
+		};
+	} catch (error) {
+		return {
+			error: true,
+			status: 400,
+			message: 'Failed to delete item.',
+		};
+	}
+};
